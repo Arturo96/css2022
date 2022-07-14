9d21e671bf750e06095a4abbfa94d41b808c439e
Carlos Arturo Vargas <carlosospino13@gmail.com>
2022-07-14 12:00:10 -0500

    Corrección de bugs al reajustar la pantalla.

--- 17-practicas/practica6/script.js
+++ 17-practicas/practica6/script.js
@@ -4,23 +4,53 @@
     menuToggle = d.getElementById("menuToggle"),
     menuDropdowns = [...d.querySelectorAll(".menu__link--dropdown")];
 
+  let isDesktop = false;
+
   menuDropdowns.forEach((dropdown) => {
     dropdown.addEventListener("click", () => {
-      const submenu = dropdown.nextElementSibling,
-        arrow = dropdown.children[0].children[0];
+      if (matchMedia("(max-width: 799px)").matches) {
+        const submenu = dropdown.nextElementSibling,
+          arrow = dropdown.children[0].children[0];
+        arrow.classList.toggle("rotate");
 
-      arrow.classList.toggle("rotate");
-      let height = 0;
+        let height = 0;
 
-      if (submenu.clientHeight == 0) {
-        height += submenu.scrollHeight;
+        if (submenu.clientHeight == 0) {
+          height += submenu.scrollHeight;
+        }
+
+        submenu.style.height = `${height}px`;
       }
-
-      submenu.style.height = `${height}px`;
     });
   });
 
   menuToggle.addEventListener("click", () => {
     menu.classList.toggle("show");
+  });
+
+  addEventListener("resize", () => {
+    if (matchMedia("(min-width: 800px)").matches && !isDesktop) {
+      // console.log("Pasó de Mobile a Desktop");
+      isDesktop = true;
+      menuDropdowns.forEach((dropdown) => {
+        const submenu = dropdown.nextElementSibling,
+          arrow = dropdown.children[0].children[0];
+        arrow.classList.remove("rotate");
+
+        submenu.style.height = `auto`;
+      });
+    }
+
+    if (matchMedia("(max-width: 799px)").matches && isDesktop) {
+      // console.log("Pasó de Desktop a Mobile");
+      isDesktop = false;
+      menuDropdowns.forEach((dropdown) => {
+        const submenu = dropdown.nextElementSibling,
+          arrow = dropdown.children[0].children[0];
+        arrow.classList.remove("rotate");
+
+        submenu.style.height = "0";
+      });
+    }
   });
 })();
