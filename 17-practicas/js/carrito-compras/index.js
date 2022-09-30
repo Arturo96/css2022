aa3253f88f7be1f383ef88032e3d5c19c00c0a2f
Carlos Arturo Vargas <carlosospino13@gmail.com>
2022-09-30 11:55:05 -0500

    Carrito implementado, falta implementar + y -.

--- 17-practicas/js/carrito-compras/index.js
+++ 17-practicas/js/carrito-compras/index.js
@@ -1,6 +1,6 @@
 const d = document;
 
-const carritoCompras = [];
+let carritoCompras = [];
 
 const existeProducto = (nombre) => {
   if (carritoCompras.length) {
@@ -15,24 +15,82 @@ const agregarProducto = (card) => {
     card.previousElementSibling.previousElementSibling.textContent;
 
   if (existeProducto(productName)) {
-    const producto = carritoCompras.find((p) => p.nombre === productName);
-    producto.cantidad++;
+    // const producto = carritoCompras.find((p) => p.nombre === productName);
+    // producto.cantidad++;
+    carritoCompras = carritoCompras.map((p) => {
+      if (p.nombre === productName) {
+        p.cantidad = p.cantidad + 1;
+      }
+      return p;
+    });
   } else {
     const productPrice = Number(
       card.previousElementSibling.textContent.slice(2)
     );
 
-    carritoCompras.push({
-      nombre: productName,
-      precio: productPrice,
-      cantidad: 1,
-    });
+    carritoCompras = [
+      ...carritoCompras,
+      {
+        nombre: productName,
+        precio: productPrice,
+        cantidad: 1,
+      },
+    ];
   }
+
+  pintarCarrito();
+};
+
+const pintarCarrito = () => {
+  const templateBody = d.getElementById("templateBody").content,
+    fragment = d.createDocumentFragment(),
+    tbody = d.getElementById("tbody"),
+    tfoot = d.getElementById("tfoot");
+
+  carritoCompras.forEach((p, index) => {
+    const clone = templateBody.cloneNode(true);
+    clone.querySelector(".id").textContent = index + 1;
+    clone.querySelector(".item").textContent = p.nombre;
+    clone.querySelector(".cantidad").textContent = p.cantidad;
+    clone.querySelector(".precio").textContent = p.precio;
+    fragment.appendChild(clone);
+  });
+
+  tbody.innerHTML = "";
+  tbody.appendChild(fragment);
+
+  const templateFooter = carritoCompras.length
+    ? d.getElementById("templateFooterTotal").content
+    : d.getElementById("templateFooterEmpty").content;
+
+  const cloneFooter = templateFooter.cloneNode(true);
+
+  if (carritoCompras.length) {
+    const cantidadTotal = carritoCompras.reduce(
+      (sum, { cantidad }) => sum + cantidad,
+      0
+    );
+
+    const precioTotal = carritoCompras.reduce(
+      (sum, { precio, cantidad }) => sum + precio * cantidad,
+      0
+    );
+
+    cloneFooter.querySelector(".cantidadTotal").textContent = cantidadTotal;
+    cloneFooter.querySelector(".precioTotal").textContent = precioTotal;
+  }
+
+  tfoot.innerHTML = "";
+  tfoot.appendChild(cloneFooter);
 };
 
 d.addEventListener("click", (e) => {
   if (e.target.matches(".btn-dark")) {
     agregarProducto(e.target);
-    console.log(carritoCompras);
+  }
+
+  if (e.target.matches(".empty")) {
+    carritoCompras = [];
+    pintarCarrito();
   }
 });
