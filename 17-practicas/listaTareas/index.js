4200b0c745ce5425f9dccb23f3712aabb9aff4f4
Carlos Arturo Vargas <carlosospino13@gmail.com>
2022-10-06 11:31:17 -0500

    IIFE

--- 17-practicas/listaTareas/index.js
+++ 17-practicas/listaTareas/index.js
@@ -1,78 +1,83 @@
-const d = document;
+(() => {
+  const d = document;
 
-const tareas = JSON.parse(localStorage.getItem("tareas")) || {};
+  const tareas = JSON.parse(localStorage.getItem("tareas")) || {};
 
-let id = Date.now();
+  let id = Date.now();
 
-const form = d.getElementById("form"),
-  fragment = d.createDocumentFragment(),
-  divTareas = d.getElementById("tareas"),
-  templateTareas = d.getElementById("templateTareas").content,
-  templateTareasEmpty = d.getElementById("templateTareasEmpty").content;
+  const form = d.getElementById("form"),
+    fragment = d.createDocumentFragment(),
+    divTareas = d.getElementById("tareas"),
+    templateTareas = d.getElementById("templateTareas").content,
+    templateTareasEmpty = d.getElementById("templateTareasEmpty").content;
 
-const pintarTareas = () => {
-  divTareas.innerHTML = "";
+  const pintarTareas = () => {
+    divTareas.innerHTML = "";
 
-  if (!Object.values(tareas).length) {
-    const clone = templateTareasEmpty.cloneNode(true);
-    fragment.appendChild(clone);
-  }
-
-  Object.values(tareas).forEach((t) => {
-    const clone = templateTareas.cloneNode(true),
-      pathIcon = t.completed
-        ? "assets/rotate-left-solid.svg"
-        : "assets/circle-check-solid.svg";
-    clone.querySelector(".tarea").className = t.completed
-      ? "tarea alert alert-primary"
-      : "tarea alert alert-warning";
-    clone.querySelector("p").textContent = t.desc;
-    if (t.completed) {
-      clone.querySelector("p").classList.add("text-decoration-line-through");
-    } else {
-      clone.querySelector("p").classList.remove("text-decoration-line-through");
+    if (!Object.values(tareas).length) {
+      const clone = templateTareasEmpty.cloneNode(true);
+      fragment.appendChild(clone);
     }
-    clone.querySelector(".check").dataset.id = t.id;
-    clone.querySelector(".check").setAttribute("src", pathIcon);
 
-    clone.querySelector(".minus").dataset.id = t.id;
-    fragment.appendChild(clone);
-  });
+    Object.values(tareas).forEach((t) => {
+      const clone = templateTareas.cloneNode(true),
+        pathIcon = t.completed
+          ? "assets/rotate-left-solid.svg"
+          : "assets/circle-check-solid.svg";
+      clone.querySelector(".tarea").className = t.completed
+        ? "tarea alert alert-primary"
+        : "tarea alert alert-warning";
+      clone.querySelector("p").textContent = t.desc;
+      if (t.completed) {
+        clone.querySelector("p").classList.add("text-decoration-line-through");
+      } else {
+        clone
+          .querySelector("p")
+          .classList.remove("text-decoration-line-through");
+      }
+      clone.querySelector(".check").dataset.id = t.id;
+      clone.querySelector(".check").setAttribute("src", pathIcon);
 
-  divTareas.appendChild(fragment);
+      clone.querySelector(".minus").dataset.id = t.id;
+      fragment.appendChild(clone);
+    });
 
-  localStorage.setItem("tareas", JSON.stringify(tareas));
-};
+    divTareas.appendChild(fragment);
 
-d.addEventListener("click", ({ target }) => {
-  if (target.matches(".check")) {
-    tareas[target.dataset.id].completed = !tareas[target.dataset.id].completed;
-    pintarTareas();
-  }
-
-  if (target.matches(".minus")) {
-    delete tareas[target.dataset.id];
-    pintarTareas();
-  }
-});
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const input = d.getElementById("input").value.trim();
-
-  if (!input.length) return;
-
-  const tarea = {
-    id: ++id,
-    desc: input,
-    completed: false,
+    localStorage.setItem("tareas", JSON.stringify(tareas));
   };
 
-  tareas[id] = { ...tarea };
-  pintarTareas();
-});
+  d.addEventListener("click", ({ target }) => {
+    if (target.matches(".check")) {
+      tareas[target.dataset.id].completed =
+        !tareas[target.dataset.id].completed;
+      pintarTareas();
+    }
 
-d.addEventListener("DOMContentLoaded", () => {
-  pintarTareas();
-});
+    if (target.matches(".minus")) {
+      delete tareas[target.dataset.id];
+      pintarTareas();
+    }
+  });
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const input = d.getElementById("input").value.trim();
+
+    if (!input.length) return;
+
+    const tarea = {
+      id: ++id,
+      desc: input,
+      completed: false,
+    };
+
+    tareas[id] = { ...tarea };
+    pintarTareas();
+  });
+
+  d.addEventListener("DOMContentLoaded", () => {
+    pintarTareas();
+  });
+})();
