bb14e85476381119f37b55c0a9a1cc031799b428
Carlos Arturo Vargas <carlosospino13@gmail.com>
2022-10-20 14:22:01 -0500

    Personalizando Tailwind CSS.

--- 18-tailwind/02-card/tailwind.config.js
+++ 18-tailwind/02-card/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ["Roboto", "ui-sans-serif"],
+      },
+      colors: {
+        danger: "#df2935",
+      },
+    },
   },
   plugins: [],
 };
