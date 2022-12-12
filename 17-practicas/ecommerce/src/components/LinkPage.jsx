c88321911f7af9a4b496f4a7f3d8798a7cafb876
Carlos Arturo Vargas <carlosospino13@gmail.com>
2022-12-12 10:21:05 -0500

    Header terminado.

--- 17-practicas/ecommerce/src/components/LinkPage.jsx
+++ 17-practicas/ecommerce/src/components/LinkPage.jsx
@@ -1,6 +1,6 @@
 const LinkPage = ({ page }) => {
   return (
-    <a href="#" className="block mb-4 lg:mb-0">
+    <a href="#" className="block mb-4 lg:mb-0 text-customBlue-dark-gray">
       {page}
     </a>
   );
